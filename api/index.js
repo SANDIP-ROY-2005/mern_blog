4ad6bdc51574f427255b6d894bf886d7c5fd8624
SANDIP-ROY-2005 <161707774+SANDIP-ROY-2005@users.noreply.github.com>
2025-01-18 17:54:44 +0530

    connected to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+ //one way to connect dot env would be 
+ //const dotenv = require('dotenv);
+ //dotenv.config(); 
+ // or simply the below mentioned 
+
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("MongoDb is connected");
+    
+}).catch(err=>{
+    console.log(err);
+});
+
 const app = express();
 app.listen(3000,()=>{
     console.log("server is running on port 3000!");
